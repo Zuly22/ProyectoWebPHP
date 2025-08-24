d88bb4ee6b78dd6f549c3ecdc5cfd36c0b4eeb31
DashSoma <daobandoso@est.utn.ac.cr>
2025-08-23 20:04:43 -0600

    Modificacion

--- index.php
+++ index.php
@@ -47,7 +47,7 @@ if (isset($_GET['buscar']) && !empty($_GET['buscar'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>UTN Solutions Real State</title>
+    <title>Inmobiliaria Pro</title>
     <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
